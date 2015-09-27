fec4943dac406c868bcd420fd5c05465a22348bd
Jackson Morgan <jaxoncreed@gmail.com>
2015-09-27 12:25:22 -0400

    Initial Commit from Restful.net

---
+++ api/models/Iuy.js
@@ -0,0 +1,20 @@
+/**
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+attributes: {
+    "k": {
+        "key": "k",
+        "autoIncrement": false,
+        "unique": false,
+        "primaryKey": false,
+        "required": false,
+        "collection": false,
+        "model": "lokiuyt"
+    }
+}
+
+};
+
---
+++ api/models/Lokiuyt.js
@@ -0,0 +1,20 @@
+/**
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+attributes: {
+    "key": {
+        "key": "key",
+        "type": "string",
+        "autoIncrement": false,
+        "unique": false,
+        "primaryKey": false,
+        "required": false,
+        "collection": false
+    }
+}
+
+};
+
